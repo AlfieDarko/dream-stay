34fdbca7002def6925752e34eafa99bc379a4c0e
Alfie Darko <me@alfiedarko.co.uk>
2018-03-28 17:38:02 +0100

    added mocha testing and removed jasmine. Began implementing post new accomodation

--- models/accommodationsModel.js
+++ models/accommodationsModel.js
@@ -1,32 +1,38 @@
-  const mongoose = require('mongoose');
-  const express = require('express');
+const mongoose = require('mongoose');
 
-  mongoose.connect('mongodb://makersacademy:123456@ds125469.mlab.com:25469/makersacademy', function(err) {
-    if (err) throw err;
 
-    console.log('Connection succesful')
+mongoose.connect('mongodb://makersacademy:123456@ds125469.mlab.com:25469/makersacademy', function(err) {
+  if (err) throw err;
 
-  });
+  console.log('Connection succesful')
+});
 
-  var accommodationSchema = mongoose.Schema({
-    // acommodationSchema to be copied here
-    id: Number,
-    hostId: Number,
-    hostName: String,
-    house: String,
-    street: String,
-    city: String,
-    description: String,
-    bedrooms: Number,
-    bathrooms: Number,
-    reviews: Array,
-    images: Array,
-    price: Number,
-    bookings: Array
-  });
+var accommodationSchema = mongoose.Schema({
+  // acommodationSchema to be copied here
+  id: Number,
+  hostId: Number,
+  hostName: String,
+  house: String,
+  street: String,
+  city: String,
+  description: String,
+  bedrooms: Number,
+  bathrooms: Number,
+  reviews: Array,
+  images: Array,
+  price: Number,
+  bookings: Array
+});
 
-  var logthis = console.log("im logging this")
 
-  var accommodationModel = mongoose.model('accommodations', accommodationSchema);
+var accommodationModel = mongoose.model('accommodations', accommodationSchema);
 
-  module.exports = accommodationModel
+var logthis = console.log("Accommodations Model Module Loaded")
+
+
+
+
+
+
+
+module.exports.accommodationModel = accommodationModel
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,41 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
-var accomSchema = require('../models/accommodationsModel.js')
+var acom = require('../models/accommodationsModel.js')
+// acom.accommodationModel(insertCollectedHouseData)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  console.log(houseData);
+  console.log(newHouse);
+
 
   res.render('index', {
     title: 'Team Awesome!!!!'
   });
 
 });
+
+
+// POST A NEW ACCOMODATION TO THE DB
+router.post('/new/accomodation', function(req, res, next) {
+
+
+  var houseData = {
+    hostName: req.body,
+    house: req.body,
+    street: req.body,
+    city: req.body,
+    description: req.body,
+    bedrooms: req.body,
+    bathrooms: req.body,
+    reviews: req.body,
+    images: req.body,
+    price: req.body,
+    bookings: req.body
+  }
+
+  var newHouse = acom.accommodationModel(houseData)
+})
 
 module.exports = router;
