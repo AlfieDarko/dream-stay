493f1123a51dbdf1864898044aa1c9c3457e8b4a
danielcampos <ddregalo@gmail.com>
2018-03-28 12:39:11 +0100

    Replace accomodationsModel into index.js and establish connection to MongoDb Atlas cloud + create git ignore for node_modules

--- controllers/accommodationsModel.js
+++ controllers/accommodationsModel.js
@@ -1,29 +1,32 @@
-const mongoose = require('moongose');
-const express = require('express');
-const app = express();
+// Would like to extra a model with the schema like below but had problems requiring files and using the variables.
 
-moongoose.connect('mongodb://makersacademy:123456@ds125469.mlab.com:25469/makersacademy', function(err) {
-  if (err) throw err;
-
-  console.log('Connection succesful')
-
-});
-
-var accommodationSchema =  moongoose.Schema({
-  // acommodationSchema to be copied here
-  id: Number,
-  hostId: Number,
-  hostName: String,
-  house: String,
-  street: String,
-  city: String,
-  description: String,
-  bedrooms: Number,
-  bathrooms: Number,
-  reviews: Array,
-  images: Array,
-  price: Number,
-  bookings: Array
-});
-
-var accommodationModel = mongoose.model ('accommodations', accommodationSchema);
+// module.exports = {
+// const mongoose = require('mongoose');
+// const express = require('express');
+//
+// mongoose.connect('mongodb://makersacademy:123456@ds125469.mlab.com:25469/makersacademy', function(err) {
+//   if (err) throw err;
+//
+//   console.log('Connection succesful')
+//
+// });
+//
+// var accommodationSchema =  mongoose.Schema({
+//   // acommodationSchema to be copied here
+//   id: Number,
+//   hostId: Number,
+//   hostName: String,
+//   house: String,
+//   street: String,
+//   city: String,
+//   description: String,
+//   bedrooms: Number,
+//   bathrooms: Number,
+//   reviews: Array,
+//   images: Array,
+//   price: Number,
+//   bookings: Array
+// });
+//
+// var accommodationModel = mongoose.model ('accommodations', accommodationSchema);
+// }
