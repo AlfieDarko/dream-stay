ed0b41f6bf426c042e7c92ee4a1c926d151329bc
danielcampos <ddregalo@gmail.com>
2018-03-28 12:03:50 +0100

    Add express serving static file - Public directory and implement bodyParser for json and urlencoded

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,8 @@
-var models = require('./accommodationsModel'); 
+var models = require('./accommodationsModel');
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false // maybe set as true for objects that are not strings or arrays?? ie. Numbers
+}));
