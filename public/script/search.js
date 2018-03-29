e8700f5ebc391858661cbed7fa868d56e2847033
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-03-29 14:57:07 +0100

    style changes

--- public/script/search.js
+++ public/script/search.js
@@ -16,36 +16,51 @@ function filterAccommodations() {
   var searchResults = accommodations.slice(0);
   if (city != '') {
     for (let i = 0; i < searchResults.length; i++) {
+      console.log('city')
+      console.log(searchResults)
       if (searchResults[i].city != city) {
         searchResults.splice(i, 1);
+        i--;
       };
     };
   }
   if (minPrice != '') {
     for (let i = 0; i < searchResults.length; i++) {
+      console.log('city')
+      console.log(searchResults)
       if (searchResults[i].price < minPrice) {
         searchResults.splice(i, 1);
+        i--;
       };
     };
   }
   if (minPrice != '') {
     for (let i = 0; i < searchResults.length; i++) {
+      console.log('city')
+      console.log(searchResults)
       if (searchResults[i].price > maxPrice) {
         searchResults.splice(i, 1);
+        i--;
       };
     };
   }
   if (baths != '') {
     for (let i = 0; i < searchResults.length; i++) {
+      console.log('city')
+      console.log(searchResults)
       if (searchResults[i].bathrooms < baths) {
         searchResults.splice(i, 1);
+        i--;
       };
     };
   }
   if (beds != '') {
     for (let i = 0; i < searchResults.length; i++) {
+      console.log('city')
+      console.log(searchResults)
       if (searchResults[i].bedrooms < beds) {
         searchResults.splice(i, 1);
+        i--;
       };
     };
   }
@@ -62,14 +77,19 @@ function list(accommodations) {
         <img class="accomodation_image" src="${accommodations[i].images[0]}">
       </div>
       <div>
-        <h1>${accommodations[i].hostName}</h1>
-        <h2> £${accommodations[i].price} per night</h2>
+        <div>
         <h2>${accommodations[i].house}</h2>
-        <h2>${accommodations[i].street}</h2>
-        <h2>${accommodations[i].city}</h2>
-        <h2>${accommodations[i].bedrooms} Bedrooms</h2>
-        <h2>${accommodations[i].bathrooms}</span> Bathrooms</h2>
+        <h2> £${accommodations[i].price} per night</h2>
+        <br></br>
+        <h3>${accommodations[i].street}</h3>
+        <h3>${accommodations[i].city}</h3>
+        <br></br>
+        <h3>${accommodations[i].bedrooms}x <img class="utility_symbol" src="http://www.clker.com/cliparts/f/D/w/D/P/6/little-bed-2-md.png">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        ${accommodations[i].bathrooms}x <img class="utility_symbol" src="https://image.flaticon.com/icons/svg/34/34730.svg"></h3>
+        <br></br>
         <button class="details_button">View Details</button>
+        </div>
       </div>
     </div>
     `
