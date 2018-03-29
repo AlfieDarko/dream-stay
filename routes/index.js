0a3594967212952f4b809d01bbb8abde3886e5bc
Alfie Darko <me@alfiedarko.co.uk>
2018-03-29 13:37:13 +0100

    Linked backend to front end, still ironing out routing links

--- routes/index.js
+++ routes/index.js
@@ -16,26 +16,50 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/search', function(req, res) {
+  res.render('search')
+})
 
+router.get('/list', function(req, res) {
+  res.render('list')
+})
 // POST A NEW ACCOMODATION TO THE DB
-router.post('/new/accomodation', function(req, res, next) {
-
+router.post('/accommodations/add', function(req, res, next) {
 
   var houseData = {
-    hostName: req.body,
-    house: req.body,
-    street: req.body,
-    city: req.body,
-    description: req.body,
-    bedrooms: req.body,
-    bathrooms: req.body,
-    reviews: req.body,
-    images: req.body,
-    price: req.body,
-    bookings: req.body
+    hostName: "admin",
+    house: req.body.house,
+    street: req.body.street,
+    city: req.body.city,
+    description: req.body.description,
+    bedrooms: parseInt(req.body.bedrooms),
+    bathrooms: parseInt(req.body.bathrooms),
+    images: [req.body.image],
+    price: parseInt(req.body.price),
   }
-
+  console.log(houseData);
   var newHouse = acom.accommodationModel(houseData)
+  newHouse.save()
+
+  console.log(newHouse, '!!!');
+  res.redirect('/search')
 })
+
+router.get('/accommodations/all', function(req, res, next) {
+  acom.accommodationModel.find(function(err, houses) {
+    if (err) {
+      res.send(err);
+    } else
+      // responds with a json object of our database
+      res.json(houses)
+  });
+})
+
+
+
+
+
+
+
 
 module.exports = router;
